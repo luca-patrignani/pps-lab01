ffc17d7fcf24972a0ee6945ba41877be1459c41a
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2025-02-24 10:32:53 +0100

    Test if the lock is initially locked

---
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
@@ -0,0 +1,45 @@
+package tdd;
+
+public class SmartDoorLockImpl implements SmartDoorLock {
+    private int pin;
+
+    @Override
+    public void setPin(int pin) {
+
+    }
+
+    @Override
+    public void unlock(int pin) {
+
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public boolean isLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return false;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return 0;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,22 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+    private SmartDoorLockImpl lock;
+
+
+    @BeforeEach
+    public void beforeEach() {
+        lock = new SmartDoorLockImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testLockIsInitiallyLocked() {
+        assertFalse(lock.isLocked());
     }
 }
